68744ce532dd683a079ba79dc15d0be3c0b9d880
BrunoG <brunoguillardopv@gmail.com>
2026-08-26 13:50:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="bos-media-v2.14";
-const ASSETS=["./","./index.html","./style.css?v=2.14","./app.js?v=2.14","./manifest.webmanifest","./icon-192.png","./icon-512.png","./logo-bruno-guillard.png","./assets/logo-bos-header.jpg"];
+const CACHE="bos-media-v2.15";
+const ASSETS=["./","./index.html","./style.css?v=2.15","./app.js?v=2.15","./manifest.webmanifest","./icon-192.png","./icon-512.png","./logo-bruno-guillard.png","./assets/logo-bos-header.jpg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener("fetch",e=>{
